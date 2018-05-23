0ee19625e3d59496559b594879bf54e2751e1c07
成凯 <hellochengkai@hotmail.com>
2018-05-23 21:51:18 +0800

    init

---
+++ app/src/main/java/com/example/chengkai/nanohttptest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chengkai.nanohttptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
